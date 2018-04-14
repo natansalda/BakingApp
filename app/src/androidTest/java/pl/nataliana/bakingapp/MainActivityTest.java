b290e0df27c64a769ed24b743f5a3ead37abd1d0
Natalia Nazaruk <nazarjazz@wp.pl>
2018-04-14 13:45:18 +0200

    Fix: dependencies problem fixed

--- app/src/androidTest/java/pl/nataliana/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/pl/nataliana/bakingapp/MainActivityTest.java
@@ -25,7 +25,7 @@ import static org.hamcrest.core.Is.is;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    public static final String RECIPE_NAME = "Cheese Cake";
+    public static final String RECIPE_NAME = "Brownies";
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
